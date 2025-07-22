07379c0c0b7c99f7b48192be8bf3023b936380c7
Yurii <98182161+YuraVitoshko@users.noreply.github.com>
2025-07-22 15:43:07 +0300

    Add files via upload

--- js/dev/index.js
+++ js/dev/index.js
@@ -4701,38 +4701,6 @@ function menuInit() {
   });
 }
 document.querySelector("[data-fls-menu]") ? window.addEventListener("load", menuInit) : null;
-function headerScroll() {
-  const header = document.querySelector("[data-fls-header-scroll]");
-  const headerShow = header.hasAttribute("data-fls-header-scroll-show");
-  const headerShowTimer = header.dataset.flsHeaderScrollShow ? header.dataset.flsHeaderScrollShow : 500;
-  const startPoint = header.dataset.flsHeaderScroll ? header.dataset.flsHeaderScroll : 1;
-  let scrollDirection = 0;
-  let timer;
-  document.addEventListener("scroll", function(e) {
-    const scrollTop = window.scrollY;
-    clearTimeout(timer);
-    if (scrollTop >= startPoint) {
-      !header.classList.contains("--header-scroll") ? header.classList.add("--header-scroll") : null;
-      if (headerShow) {
-        if (scrollTop > scrollDirection) {
-          header.classList.contains("--header-show") ? header.classList.remove("--header-show") : null;
-        } else {
-          !header.classList.contains("--header-show") ? header.classList.add("--header-show") : null;
-        }
-        timer = setTimeout(() => {
-          !header.classList.contains("--header-show") ? header.classList.add("--header-show") : null;
-        }, headerShowTimer);
-      }
-    } else {
-      header.classList.contains("--header-scroll") ? header.classList.remove("--header-scroll") : null;
-      if (headerShow) {
-        header.classList.contains("--header-show") ? header.classList.remove("--header-show") : null;
-      }
-    }
-    scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
-  });
-}
-document.querySelector("[data-fls-header-scroll]") ? window.addEventListener("load", headerScroll) : null;
 class ScrollWatcher {
   constructor(props) {
     let defaultConfig = {
@@ -4976,7 +4944,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   function validateFullName(fullName) {
     const words = fullName.trim().split(/\s+/);
-    if (words.length < 2) return false;
+    if (words.length < 1) return false;
     return words.every((word) => /^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ']{2,}$/.test(word));
   }
 });
